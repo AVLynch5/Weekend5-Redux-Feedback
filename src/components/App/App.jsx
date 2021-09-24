504297833805db64bb367433cdd6f98d39c0007e
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-24 14:53:18 -0400

    imported Router Route and Link into app.jsx. Imported Home.jsx into app.jsx. Added Router element with routes for each component.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,12 +4,15 @@ import './App.css';
 //import useSelector
 import { useSelector } from 'react-redux';
 //import components
+import Home from '../Form1/Home';
 import Form1 from '../Form1/Form1';
 import Form2 from '../Form2/Form2';
 import Form3 from '../Form3/Form3';
 import Form4 from '../Form4/Form4';
 import Review from '../Review/Review';
 import Success from '../Success/Success';
+//import Router, Route, Link for hash routing
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
 
@@ -23,12 +26,29 @@ function App() {
         <h4>Don't forget it!</h4>
       </header>
       <p>{JSON.stringify(reduxStore)}</p>
-      <Form1 />
-      <Form2 />
-      <Form3 />
-      <Form4 />
-      <Review />
-      <Success />
+      <Router>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/Form1">
+          <Form1 />
+        </Route>
+        <Route path="/Form2">
+          <Form2 />
+        </Route>
+        <Route path="/Form3">
+          <Form3 />
+        </Route>
+        <Route path="/Form4">
+          <Form4 />
+        </Route>
+        <Route path="/Review">
+          <Review />
+        </Route>
+        <Route path="/Success">
+          <Success />
+        </Route>
+      </Router>
     </div>
   );
 }
