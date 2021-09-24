55c12c7b426a898fd8e7901d134117ef9d60d248
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-24 09:55:55 -0400

    imported feedback.router.js into server.js and called it using app.use

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+//import router
+const feedbackRouter = require('./routes/feedback.router.js');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); 
@@ -9,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+app.use('/feedback', feedbackRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
