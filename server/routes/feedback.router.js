ba71d9b600be98fe3e9b02489e0d82da98c622e8
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-24 10:37:13 -0400

    Added POST route to feedback.router for adding feedback

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -7,7 +7,20 @@ const pool = require('../modules/pool.js');
 
 //GET
 
-//POST
+//POST - add feedback obj to DB
+router.post('/', (req, res) => {
+    const newFeedback = req.body;
+    const queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+                       VALUES ($1, $2, $3, $4)`;
+    pool.query(queryText, [newFeedback.form1, newFeedback.form2, newFeedback.form3, newFeedback.form4]
+    ).then((results) => {
+        console.log('Added new feedback!');
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log('Error adding new feedback via POST', error);
+        res.sendStatus(500);
+    })
+})
 
 //DELETE
 
