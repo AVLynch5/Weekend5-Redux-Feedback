e1e1518b404eda11df20a51ecf013fd9f519875e
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-24 09:03:28 -0400

    updated form 4

--- src/components/Form4/Form4.jsx
+++ src/components/Form4/Form4.jsx
@@ -7,9 +7,9 @@ import { useSelector } from "react-redux";
 //import useHistory
 import { useHistory } from "react-router";
 
-function Form1() {
+function Form4() {
     //declare state variable for form data - for clearing form after submission
-    //const [form1Data, setForm1Data] = useState('');
+    //const [form4Data, setForm4Data] = useState('');
 
     //declare dispatch
     const dispatch = useDispatch();
@@ -22,8 +22,8 @@ function Form1() {
         //prevent default form behavior 
         event.preventDefault();
         //conditional to check user input - input validation
-        if (feedbackobj.form1 > 0 && feedbackobj.form1 < 6) {
-            //const action = {type: 'SET_FORM_1', payload: feedbackobj.form1}; - this is already done! If success, route to next page and retain value.
+        if (feedbackobj.form4 > 0 && feedbackobj.form4 < 6) {
+            //const action = {type: 'SET_FORM_4', payload: feedbackobj.form4}; - this is already done! If success, route to next page and retain value.
             //dispatch action
             //dispatch(action);
             //history.push(route); to redirect user to next form
@@ -31,18 +31,18 @@ function Form1() {
             alert('Please enter a valid number between 1 and 5');
             return;
         }
-        //setForm1Data(''); - no longer want to clear input after submit button. If we return to this page, user should see entered value and be able to change.
+        //setForm4Data(''); - no longer want to clear input after submit button. If we return to this page, user should see entered value and be able to change.
         
     }
     return(
         <>
-        <h2 className="page-header"></h2>
+        <h2 className="page-header">Any comments you want to leave?</h2>
         <form className="form" onSubmit={handleSubmit}>
-            <input required placeholder="Feeling?" value={feedbackobj.form1} onChange={(event) => dispatch({type: 'SET_FORM_1', payload: event.target.value})} />
+            <input placeholder="Comments?" value={feedbackobj.form4} onChange={(event) => dispatch({type: 'SET_FORM_4', payload: event.target.value})} />
             <button type="submit">Next</button>
         </form>
         </>
     );
 }
 
-export default Form1;
+export default Form4;
