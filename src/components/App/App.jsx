c0783004b1fc6761f27d59f4b781a9066b0a13ea
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-24 09:27:13 -0400

    imported Review.jsx into app.jsx and called it under Form components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,6 +8,7 @@ import Form1 from '../Form1/Form1';
 import Form2 from '../Form2/Form2';
 import Form3 from '../Form3/Form3';
 import Form4 from '../Form4/Form4';
+import Review from '../Review/Review';
 
 function App() {
 
@@ -25,6 +26,7 @@ function App() {
       <Form2 />
       <Form3 />
       <Form4 />
+      <Review />
     </div>
   );
 }
