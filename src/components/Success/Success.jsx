4fa6be9b20caeeadf9fec97271dec4553b0d9149
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-24 14:23:56 -0400

    updated Success.jsx - on click button calls handleSubmissionSuccess. handleSubmissionSuccess declares an action and dispatches that action to clear tempFeedbackReducer and routes to Form1

---
+++ src/components/Success/Success.jsx
@@ -0,0 +1,30 @@
+//import useDispatch;
+import { useDispatch } from "react-redux";
+//import useHistory;
+import { useHistory } from "react-router";
+
+function Success() {
+    //declare dispatch
+    const dispatch = useDispatch();
+    //declare history
+    const history = useHistory();
+    //function handleSubmissionSuccess - declare action to wipe tempFeedbackReducer and route to Form1
+    const handleSubmissionSuccess = () => {
+        const action = {type: 'CLEAR_TEMPFB'};
+        dispatch(action);
+        //history.push(Form1);
+    }
+    return(
+        <>
+            <div>
+                <h1>Feedback submitted successfully!</h1>
+            </div>
+            <div>
+                <p>Thank you for your feedback!</p>
+                <button onClick={handleSubmissionSuccess}>Leave New Feedback</button>
+            </div>
+        </>
+    );
+}
+
+export default Success;
