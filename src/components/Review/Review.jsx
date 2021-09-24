9a96d3540468d3ce6a748c13fc056824fe492958
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-24 09:26:16 -0400

    Review.jsx - imported useSelector. USed span elements to display form properties stored in tempFeedbackReducer

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,8 +1,21 @@
+//import useSelector to access stored data
+import { useSelector } from "react-redux";
+
 function Review() {
+    //declare store to access temp info
+    tempObj = useSelector(store => store.tempFeedbackReducer);
 
     return(
         <>
-
+            <h2 className="page-header">Review Your Feedback</h2>
+            <br/><br/>
+            <p>Feeling: <span>{tempObj.Form1}</span></p>
+            <br/>
+            <p>Understanding: <span>{tempObj.Form2}</span></p>
+            <br/>
+            <p>Support: <span>{tempObj.Form3}</span></p>
+            <br/>
+            <p>Comments: <span>{tempObj.Form4}</span></p>
         </>
     );
 }
