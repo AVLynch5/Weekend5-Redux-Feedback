dec81ccfd76bf6211f7aa75ea5e4f9dd3ef2584c
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-09-24 09:53:31 -0400

    created routes folder within server and created feedback.router.js

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,14 @@
+//feedback.router.js
+
+const express = require('express');
+const router = express.Router();
+//to communicate with database
+const pool = require('../modules/pool.js');
+
+//GET
+
+//POST
+
+//DELETE
+
+module.exports = router;
